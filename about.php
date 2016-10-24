6cf828cc9ee5b3214f343d212254ed2690170f3d
alexbaltser <ibaltser@gmail.com>
2016-10-24 17:49:53 +0300

    Update about.php

--- about.php
+++ about.php
@@ -1,7 +1,41 @@
 <? php
 
-
-
+$firstname="Алексей";
+$lastname="Бальцер";
+$yearofbirth="1987";
+$currentyear="2016";
+$age=$currentyear-$yearofbirth;
+$name=$firstname . " " . $lastname;
+$city="Краснодар";
+$about="Мягкий и пушистый";
 ?>
 
-<h1>Страница пользователя <? $name ?>
+<body>
+
+<h1>Страница пользователя <? $name ?></h1>
+<dl>
+  <dt>Имя</dt>
+  <dd><? $firstname ?></dd>
+</dl>
+
+<dl>
+  <dt>Возраст</dt>
+  <dd><? $age ?> </dd>
+</dl>
+
+<dl>
+  <dt>Адрес электронной почты</dt>
+  <dd>ibaltser@gmail.com</dd>  
+</dl>
+
+<dl>
+  <dt>Город</dt>
+  <dd><? $city ?></dd>
+</dl>
+
+<dl>
+  <dt>О себе</dt>
+  <dd><? $about ?></dd>
+</dl>
+  
+ </body>
