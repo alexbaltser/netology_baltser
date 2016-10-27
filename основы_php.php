68001a9cf2a4d8e31b3680aa03a53a67da0fbcd0
alexbaltser <ibaltser@gmail.com>
2016-10-27 16:40:20 +0300

    Create основы_php.php

---
+++ основы_php.php
@@ -0,0 +1,31 @@
+<html>
+<head><title>Домашнее задание 2</title></head>
+<body>
+
+<p>
+<?php
+
+$number="A";
+$user_number="B";
+
+if ($user_number > $number) {
+    echo "много";
+}
+
+else {
+    if ($user_number < $number) {
+        echo "мало";
+    }
+
+    else {
+        echo "вы угадали";
+    }
+
+}
+
+
+ ?>
+
+</p>
+</body>
+</html>
